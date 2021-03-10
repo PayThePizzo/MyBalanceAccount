a38dd914625639b3de035b81e18a563c21b18707
PayThePizzo <44942410+PayThePizzo@users.noreply.github.com>
2021-03-10 19:04:27 +0100

    Spring Initializr

---
+++ src/main/java/org/example/MyAccountBalance/MyAccountBalanceApplication.java
@@ -0,0 +1,13 @@
+package org.example.MyAccountBalance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAccountBalanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAccountBalanceApplication.class, args);
+	}
+
+}
